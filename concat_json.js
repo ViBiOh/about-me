33a640034a31404197c9c9902f06053eaa57c165
Vincent Boutour <bob@vibioh.fr>
2019-12-16 10:07:30 +0100

    Adding schema validator

--- concat_json.js
+++ concat_json.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const utils = require('js-utils');
+const resumeSchema = require('resume-schema');
 const dateFns = require('date-fns');
 const dateFnsLocale = require('date-fns/locale');
 
@@ -23,27 +24,43 @@ function displayError(error) {
   process.exit(1);
 }
 
+function computeDates(payload) {
+  payload.work.forEach(work => {
+    if (work.startDate) {
+      try {
+        const startDate = new Date(work.startDate);
+        const endDate = work.endDate ? new Date(work.endDate) : new Date();
+
+        work.duration = dateFns.formatDistance(startDate, endDate, { locale: dateFnsLocale.fr });
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+
+  return payload;
+}
+
+function validateSchema(payload) {
+  resumeSchema.validate(payload, err => {
+    if (err) {
+      console.error('The resume is invalid:', err);
+    }
+  });
+
+  return payload;
+}
+
 new Promise((resolve, reject) => {
   Promise.all(process.argv.slice(2).map(json => promiseReadFile(json, UTF_8)))
+    .then(content => content.map(c => JSON.parse(c)))
     .then(files => {
-      const concat = files.reduce((p, c) => Object.assign(p, JSON.parse(c)), {});
-
-      // computing date delta
-      concat.work.forEach(work => {
-        if (work.startDate) {
-          try {
-            const startDate = new Date(work.startDate);
-            const endDate = work.endDate ? new Date(work.endDate) : new Date();
-
-            work.duration = dateFns.formatDistance(startDate, endDate, { locale: dateFnsLocale.fr });
-          } catch (e) {
-            console.error(e);
-          }
-        }
-      });
-
-      resolve(JSON.stringify(concat, null, 2));
+      validateSchema(files[0]);
+      return files;
     })
+    .then(files => files.reduce((p, c) => Object.assign(p, c), {}))
+    .then(computeDates)
+    .then(payload => resolve(JSON.stringify(payload, null, 2)))
     .catch(reject);
 })
   .then(displaySuccess)
