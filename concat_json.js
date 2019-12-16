8bfa9de842ad97afadf0660476646fe3dfa00a9c
Vincent Boutour <bob@vibioh.fr>
2019-12-16 09:28:29 +0100

    Adding date computation

--- concat_json.js
+++ concat_json.js
@@ -2,6 +2,9 @@
 
 const fs = require('fs');
 const utils = require('js-utils');
+const dateFns = require('date-fns');
+const dateFnsLocale = require('date-fns/locale');
+
 const UTF_8 = 'utf-8';
 
 const promiseReadFile = utils.asyncifyCallback(fs.readFile);
@@ -24,6 +27,21 @@ new Promise((resolve, reject) => {
   Promise.all(process.argv.slice(2).map(json => promiseReadFile(json, UTF_8)))
     .then(files => {
       const concat = files.reduce((p, c) => Object.assign(p, JSON.parse(c)), {});
+
+      // computing date delta
+      concat.work.forEach(work => {
+        if (work.startDate) {
+          try {
+            const startDate = new Date(work.startDate);
+            const endDate = work.endDate ? new Date(work.endDate) : new Date();
+
+            work.duration = dateFns.formatDistance(startDate, endDate, { locale: dateFnsLocale.fr });
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      });
+
       resolve(JSON.stringify(concat, null, 2));
     })
     .catch(reject);
