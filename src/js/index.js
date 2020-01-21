f0f751e5076da3a07c1e135a9ea52eec43514908
Vincent Boutour <bob@vibioh.fr>
2020-01-21 10:38:08 +0100

    chore: Cleanup and formating code

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 function toggleKeyboard(event) {
-  if ((event.code === 'Enter' || event.code === 'Space')) {
+  if (event.code === 'Enter' || event.code === 'Space') {
     var element = document.querySelector('#' + event.target.htmlFor);
     if (element) {
       event.preventDefault();
