ccd01d77d1547a4531db231a61460c73b013f723
Vincent Boutour <bob@vibioh.fr>
2022-08-14 11:19:39 +0200

    refactor: Using const instead of old var
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 function toggleKeyboard(event) {
-  if (event.code === 'Enter' || event.code === 'Space') {
-    var element = document.querySelector('#' + event.target.htmlFor);
+  if (event.code === "Enter" || event.code === "Space") {
+    const element = document.querySelector("#" + event.target.htmlFor);
     if (element) {
       event.preventDefault();
       element.checked = !element.checked;
